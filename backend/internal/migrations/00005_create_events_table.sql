678c2613180c8404115c76adfe0c5c85b76876e0
sunsetsavorer <sunset.savorer@gmail.com>
2026-03-10 20:00:49 +0300

    [feat/backend-skills] migration: global refactor

--- backend/internal/migrations/00005_create_events_table.sql
+++ backend/internal/migrations/00005_create_events_table.sql
@@ -6,6 +6,11 @@ CREATE TABLE IF NOT EXISTS events(
 	user_id UUID NOT NULL
 		REFERENCES users(id) ON DELETE CASCADE,
 	source TEXT NOT NULL,
+	event_type_id UUID NOT NULL
+		REFERENCES event_types(id) ON DELETE CASCADE,
+	has_impact BOOLEAN NOT NULL,
+	is_hard BOOLEAN NOT NULL,
+	is_new BOOLEAN NOT NULL,
 	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 -- +goose StatementEnd
--- backend/internal/migrations/00006_create_event_rewards_table.sql
+++ backend/internal/migrations/00006_create_event_rewards_table.sql
@@ -1,13 +1,13 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS event_rewards(
+	id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 	event_id UUID NOT NULL
 		REFERENCES events(id) ON DELETE CASCADE,
-	skill_id UUID NOT NULL
-		REFERENCES skills(id) ON DELETE CASCADE,
+	user_skill_id UUID NOT NULL
+		REFERENCES user_skills(id) ON DELETE CASCADE,
 	xp_amount INTEGER NOT NULL,
-	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-	PRIMARY KEY (event_id, skill_id)
+	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 -- +goose StatementEnd
 
