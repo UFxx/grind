a98a10ab6638f85f1d4073714f96032ad0b9b03d
sunsetsavorer <sunset.savorer@gmail.com>
2026-03-19 21:58:30 +0300

    [feat/frontend-delete-invite-code] feat: delete invite code

--- frontend/app/api/user.ts
+++ frontend/app/api/user.ts
@@ -1,5 +1,7 @@
 import { type UserResponse } from "~/types/user"
 
 export default {
-	getProfile: async () => await useRequest<UserResponse>('/users/me/profile')
+	getProfile: async () => await useRequest<UserResponse>('/users/me/profile'),
+
+	deleteInviteCodeByID: async (id: string) => await useRequest(`/users/me/invite-codes/${id}`, { method: 'DELETE' })
 };
