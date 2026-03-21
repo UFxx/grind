78be7954947ad80a0d4a4dc0a95891f7c9a5bc72
sunsetsavorer <sunset.savorer@gmail.com>
2026-03-21 20:18:34 +0300

    [feat/frontend-toaster] feat: toast store

---
+++ frontend/app/stores/toast.ts
@@ -0,0 +1,34 @@
+import { defineStore } from 'pinia';
+import { type Toast, type ToastType } from '../types/toast';
+
+export const useToastStore = defineStore('toast', () =>
+	{
+		const toasts = ref<Toast[]>([]);
+		const innerID = ref(0);
+
+		const getToasts = () => toasts.value;
+
+		const addToast = (type: ToastType, text: string) =>
+		{
+			const toast: Toast = {
+				id: getNextID(),
+				type: type,
+				text: text
+			};
+
+			toasts.value.push(toast);
+
+			setTimeout(() => removeToast(toast.id), 3000);
+		}
+
+		const removeToast = (id: number) => toasts.value = toasts.value.filter((toast: Toast) => toast.id !== id);
+
+		const getNextID = () => innerID.value++;
+
+		return {
+			getToasts,
+			addToast,
+			removeToast
+		};
+	}
+)
