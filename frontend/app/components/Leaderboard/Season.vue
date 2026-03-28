634c8d1455b5bf6917fb250ce7acc0cb1b377546
sunsetsavorer <sunset.savorer@gmail.com>
2026-03-28 13:59:41 +0300

    [feat/frontend-leaderboard] feat: leaderboard components

---
+++ frontend/app/components/Leaderboard/Season.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+	import type { FormattedSeason } from '~/types/leaderboard';
+
+	const props = defineProps<{ season: FormattedSeason }>();
+
+</script>
+
+<template>
+</template>
+
+<style lang="scss">
+</style>
